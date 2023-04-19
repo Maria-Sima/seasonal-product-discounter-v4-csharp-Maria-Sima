c7d12a5cb68030df486c885245a09841cbdc955c
appuser <appuser@journey2-d5476bc96-qd54r>
2023-04-19 07:00:55 +0000

    starter project

---
+++ CodeCool.SeasonalProductDiscounter/Service/Logger/FileLogger.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeCool.SeasonalProductDiscounter.Service.Logger;
+
+public class FileLogger : LoggerBase
+{
+    private readonly string _logFile;
+
+    public FileLogger(string logFile)
+    {
+        _logFile = logFile;
+    }
+
+    protected override void LogMessage(string message, string type)
+    {
+        //use File.AppendText to write messages
+    }
+}
